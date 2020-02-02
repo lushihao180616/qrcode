0a9a0170a5451309291685509c41e79a7e0aed9a
Administrator <lushihaomb@qq.com>
2020-02-02 23:46:01 +0800

    添加统计

--- src/main/java/com/lushihao/qrcode/dao/QRCodeRecordMapper.java
+++ src/main/java/com/lushihao/qrcode/dao/QRCodeRecordMapper.java
@@ -1,6 +1,7 @@
 package com.lushihao.qrcode.dao;
 
 import com.lushihao.qrcode.entity.qrcode.QRCodeRecord;
+import com.lushihao.qrcode.entity.temple.TempleAnalysis;
 import org.apache.ibatis.annotations.Mapper;
 
 import java.util.List;
@@ -11,5 +12,7 @@ public interface QRCodeRecordMapper {
     void create(QRCodeRecord qrCodeRecord);
 
     List<QRCodeRecord> select(QRCodeRecord qrCodeRecord);
+
+    List<TempleAnalysis> selectTempleAnalysis(String date1, String date2);
 
 }
