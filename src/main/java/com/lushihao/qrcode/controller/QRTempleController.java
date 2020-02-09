70ade2df75e84f6dd35827d87593d78e6df5c629
lushihao <lushihaomb@qq.com>
2020-02-09 20:48:17 +0800

    更一版代码

--- src/main/java/com/lushihao/qrcode/controller/QRTempleController.java
+++ src/main/java/com/lushihao/qrcode/controller/QRTempleController.java
@@ -28,89 +28,8 @@ public class QRTempleController {
         if(checkController.check().equals("0")){
             return null;
         }
+        QRCodeTemple qrCodeTemple = translate(reqMap);
         String templeItemsPath = (String) reqMap.get("templeItemsPath");
-        QRCodeTemple qrCodeTemple = LSHMapUtils.mapToEntity(reqMap, QRCodeTemple.class);
-        String code = (String) reqMap.get("code");
-        if (code.charAt(0) == 'J') {
-            qrCodeTemple.setIfGif(false);
-        } else if (code.charAt(0) == 'D') {
-            qrCodeTemple.setIfGif(true);
-        }
-        if (code.charAt(1) == 'W') {
-            qrCodeTemple.setIfShowLogo(false);
-        } else if (code.charAt(1) == 'Y') {
-            qrCodeTemple.setIfShowLogo(true);
-        }
-        if (code.charAt(2) == '0') {
-            qrCodeTemple.setIfOnly(true);
-        } else if (code.charAt(2) == '1') {
-            qrCodeTemple.setIfOnly(false);
-        }
-        if (code.charAt(3) == '0') {
-            qrCodeTemple.setArti("0-1-2-3-4");
-        } else if (code.charAt(3) == '1') {
-            qrCodeTemple.setArti("0-1-2-5-6");
-        }
-        if (code.charAt(2) == '1' && code.charAt(4) == '1') {
-            qrCodeTemple.setIfSelfBg(true);
-        } else {
-            qrCodeTemple.setIfSelfBg(false);
-        }
-        qrCodeTemple.setMoney(Double.parseDouble(reqMap.get("money").toString()));
-        if (reqMap.get("width") == null) {
-            qrCodeTemple.setWidth(0);
-        } else {
-            qrCodeTemple.setWidth((Integer) reqMap.get("width"));
-        }
-        if (reqMap.get("height") == null) {
-            qrCodeTemple.setHeight(0);
-        } else {
-            qrCodeTemple.setHeight((Integer) reqMap.get("height"));
-        }
-        if (reqMap.get("x") == null) {
-            qrCodeTemple.setX(0);
-        } else {
-            qrCodeTemple.setX((Integer) reqMap.get("x"));
-        }
-        if (reqMap.get("y") == null) {
-            qrCodeTemple.setY(0);
-        } else {
-            qrCodeTemple.setY((Integer) reqMap.get("y"));
-        }
-        qrCodeTemple.setIconNum((Integer) reqMap.get("iconNum"));
-        if (reqMap.get("angle") == null || "".equals(reqMap.get("angle"))) {
-            qrCodeTemple.setAngle(0);
-        } else {
-            qrCodeTemple.setAngle((Integer) reqMap.get("angle"));
-        }
-        if (reqMap.get("multiple") == null) {
-            qrCodeTemple.setMultiple(0);
-        } else {
-            qrCodeTemple.setMultiple((Integer) reqMap.get("multiple"));
-        }
-        if (qrCodeTemple.isIfGif()) {
-            qrCodeTemple.setIfGif(true);
-            String frame = (String) reqMap.get("frame");
-            if (frame == null || "".equals(frame)) {
-                qrCodeTemple.setFrame(8);
-                qrCodeTemple.setStartQRFrame(0);
-                qrCodeTemple.setEndQRFrame(0);
-            } else {
-                String[] frames = frame.split("/");
-                qrCodeTemple.setFrame(Integer.valueOf(frames[0]));
-                if (frames.length > 1) {
-                    qrCodeTemple.setStartQRFrame(Integer.valueOf(frames[1]));
-                }
-                if (frames.length > 2) {
-                    qrCodeTemple.setEndQRFrame(Integer.valueOf(frames[2]));
-                }
-            }
-        } else {
-            qrCodeTemple.setIfGif(false);
-            qrCodeTemple.setFrame(0);
-            qrCodeTemple.setStartQRFrame(0);
-            qrCodeTemple.setEndQRFrame(0);
-        }
         String back = qrTempleService.create(qrCodeTemple, templeItemsPath);
         return back;
     }
@@ -121,89 +40,8 @@ public class QRTempleController {
         if(checkController.check().equals("0")){
             return null;
         }
+        QRCodeTemple qrCodeTemple = translate(reqMap);
         String templeItemsPath = (String) reqMap.get("templeItemsPath");
-        QRCodeTemple qrCodeTemple = LSHMapUtils.mapToEntity(reqMap, QRCodeTemple.class);
-        String code = (String) reqMap.get("code");
-        if (code.charAt(0) == 'J') {
-            qrCodeTemple.setIfGif(false);
-        } else if (code.charAt(0) == 'D') {
-            qrCodeTemple.setIfGif(true);
-        }
-        if (code.charAt(1) == 'W') {
-            qrCodeTemple.setIfShowLogo(false);
-        } else if (code.charAt(1) == 'Y') {
-            qrCodeTemple.setIfShowLogo(true);
-        }
-        if (code.charAt(2) == '0') {
-            qrCodeTemple.setIfOnly(true);
-        } else if (code.charAt(2) == '1') {
-            qrCodeTemple.setIfOnly(false);
-        }
-        if (code.charAt(3) == '0') {
-            qrCodeTemple.setArti("0-1-2-3-4");
-        } else if (code.charAt(3) == '1') {
-            qrCodeTemple.setArti("0-1-2-5-6");
-        }
-        if (code.charAt(2) == '1' && code.charAt(4) == '1') {
-            qrCodeTemple.setIfSelfBg(true);
-        } else {
-            qrCodeTemple.setIfSelfBg(false);
-        }
-        qrCodeTemple.setMoney(Double.parseDouble(reqMap.get("money").toString()));
-        if (reqMap.get("width") == null) {
-            qrCodeTemple.setWidth(0);
-        } else {
-            qrCodeTemple.setWidth((Integer) reqMap.get("width"));
-        }
-        if (reqMap.get("height") == null) {
-            qrCodeTemple.setHeight(0);
-        } else {
-            qrCodeTemple.setHeight((Integer) reqMap.get("height"));
-        }
-        if (reqMap.get("x") == null) {
-            qrCodeTemple.setX(0);
-        } else {
-            qrCodeTemple.setX((Integer) reqMap.get("x"));
-        }
-        if (reqMap.get("y") == null) {
-            qrCodeTemple.setY(0);
-        } else {
-            qrCodeTemple.setY((Integer) reqMap.get("y"));
-        }
-        qrCodeTemple.setIconNum((Integer) reqMap.get("iconNum"));
-        if (reqMap.get("angle") == null || "".equals(reqMap.get("angle"))) {
-            qrCodeTemple.setAngle(0);
-        } else {
-            qrCodeTemple.setAngle((Integer) reqMap.get("angle"));
-        }
-        if (reqMap.get("multiple") == null) {
-            qrCodeTemple.setMultiple(0);
-        } else {
-            qrCodeTemple.setMultiple((Integer) reqMap.get("multiple"));
-        }
-        if (qrCodeTemple.isIfGif()) {
-            qrCodeTemple.setIfGif(true);
-            String frame = (String) reqMap.get("frame");
-            if (frame == null || "".equals(frame)) {
-                qrCodeTemple.setFrame(8);
-                qrCodeTemple.setStartQRFrame(0);
-                qrCodeTemple.setEndQRFrame(0);
-            } else {
-                String[] frames = frame.split("/");
-                qrCodeTemple.setFrame(Integer.valueOf(frames[0]));
-                if (frames.length > 1) {
-                    qrCodeTemple.setStartQRFrame(Integer.valueOf(frames[1]));
-                }
-                if (frames.length > 2) {
-                    qrCodeTemple.setEndQRFrame(Integer.valueOf(frames[2]));
-                }
-            }
-        } else {
-            qrCodeTemple.setIfGif(false);
-            qrCodeTemple.setFrame(0);
-            qrCodeTemple.setStartQRFrame(0);
-            qrCodeTemple.setEndQRFrame(0);
-        }
         String back = qrTempleService.update(qrCodeTemple, templeItemsPath);
         return back;
     }
@@ -230,6 +68,97 @@ public class QRTempleController {
             code = null;
         }
         return qrTempleService.filter(code);
+    }
+
+    /**
+     * 前台转后台
+     * @param reqMap
+     * @return
+     */
+    private QRCodeTemple translate(Map<String, Object> reqMap){
+        QRCodeTemple qrCodeTemple = LSHMapUtils.mapToEntity(reqMap, QRCodeTemple.class);
+        String code = (String) reqMap.get("code");
+        if (code.charAt(0) == 'J') {
+            qrCodeTemple.setIfGif(false);
+        } else if (code.charAt(0) == 'D') {
+            qrCodeTemple.setIfGif(true);
+        }
+        if (code.charAt(1) == 'W') {
+            qrCodeTemple.setIfShowLogo(false);
+        } else if (code.charAt(1) == 'Y') {
+            qrCodeTemple.setIfShowLogo(true);
+        }
+        if (code.charAt(2) == '0') {
+            qrCodeTemple.setIfOnly(true);
+        } else if (code.charAt(2) == '1') {
+            qrCodeTemple.setIfOnly(false);
+        }
+        if (code.charAt(3) == '0') {
+            qrCodeTemple.setArti("0-1-2-3-4");
+        } else if (code.charAt(3) == '1') {
+            qrCodeTemple.setArti("0-1-2-5-6");
+        }
+        if (code.charAt(2) == '1' && code.charAt(4) == '1') {
+            qrCodeTemple.setIfSelfBg(true);
+        } else {
+            qrCodeTemple.setIfSelfBg(false);
+        }
+        qrCodeTemple.setMoney(Double.parseDouble(reqMap.get("money").toString()));
+        if (reqMap.get("width") == null) {
+            qrCodeTemple.setWidth(0);
+        } else {
+            qrCodeTemple.setWidth((Integer) reqMap.get("width"));
+        }
+        if (reqMap.get("height") == null) {
+            qrCodeTemple.setHeight(0);
+        } else {
+            qrCodeTemple.setHeight((Integer) reqMap.get("height"));
+        }
+        if (reqMap.get("x") == null) {
+            qrCodeTemple.setX(0);
+        } else {
+            qrCodeTemple.setX((Integer) reqMap.get("x"));
+        }
+        if (reqMap.get("y") == null) {
+            qrCodeTemple.setY(0);
+        } else {
+            qrCodeTemple.setY((Integer) reqMap.get("y"));
+        }
+        qrCodeTemple.setIconNum((Integer) reqMap.get("iconNum"));
+        if (reqMap.get("angle") == null || "".equals(reqMap.get("angle"))) {
+            qrCodeTemple.setAngle(0);
+        } else {
+            qrCodeTemple.setAngle((Integer) reqMap.get("angle"));
+        }
+        if (reqMap.get("multiple") == null) {
+            qrCodeTemple.setMultiple(0);
+        } else {
+            qrCodeTemple.setMultiple((Integer) reqMap.get("multiple"));
+        }
+        if (qrCodeTemple.isIfGif()) {
+            qrCodeTemple.setIfGif(true);
+            String frame = (String) reqMap.get("frame");
+            if (frame == null || "".equals(frame)) {
+                qrCodeTemple.setFrame(8);
+                qrCodeTemple.setStartQRFrame(0);
+                qrCodeTemple.setEndQRFrame(0);
+            } else {
+                String[] frames = frame.split("/");
+                qrCodeTemple.setFrame(Integer.valueOf(frames[0]));
+                if (frames.length > 1) {
+                    qrCodeTemple.setStartQRFrame(Integer.valueOf(frames[1]));
+                }
+                if (frames.length > 2) {
+                    qrCodeTemple.setEndQRFrame(Integer.valueOf(frames[2]));
+                }
+            }
+        } else {
+            qrCodeTemple.setIfGif(false);
+            qrCodeTemple.setFrame(0);
+            qrCodeTemple.setStartQRFrame(0);
+            qrCodeTemple.setEndQRFrame(0);
+        }
+        return qrCodeTemple;
     }
 
 }
